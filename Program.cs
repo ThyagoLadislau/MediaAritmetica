9afed6c92465022eba8a35415423ca4816d63ece
Thyago <thyagothyago600@gmail.com>
2025-11-28 21:57:43 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tirar a media");
+Console.WriteLine("Primeira Nota :");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("segunda Nota :");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("terceira Nota :");
+int n3 = int.Parse(Console.ReadLine());
+int media = (n1 + n2 + n3 ) / 3 ;
+Console.WriteLine($"A media é {media}");
